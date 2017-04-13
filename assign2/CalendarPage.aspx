92caf01266c9fd3f71f0fd20f20363c61f6ed5be
Jess <jessallen6@hotmail.com>
2017-04-13 12:05:27 -0700

    Calendar!

---
+++ assign2/CalendarPage.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CalendarPage.aspx.cs" Inherits="CalendarPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+    <div id="centered">
+        <h1>Calendar 2017</h1>
+        <asp:Calendar ID="Calendar1" cssClass="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged"></asp:Calendar>
+        <asp:Label ID="Label1" runat="server" cssClass="result" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ assign2/CalendarPage.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CalendarPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+    {
+        Label1.Text = "";
+        string[,] events = new string[5, 2];
+
+        events[0, 0] = "4/24/2017";
+        events[0, 1] = "RVL Private Party- 8:00pm";
+        events[1, 0] = "4/24/2017";
+        events[1, 1] = "Magic Hat in the Grotto- 7:30pm";
+        events[2, 0] = "4/24/2017";
+        events[2, 1] = "Karaoke with El- 9:30pm";
+        events[3, 0] = "4/25/2017";
+        events[3, 1] = "Comedy Nest in the Grotto- 7:30pm";
+        events[4, 0] = "4/26/2017";
+        events[4, 1] = "Fist and Shout- Comedy- 9:00pm";
+
+
+        string dates = Calendar1.SelectedDate.ToShortDateString();
+
+        for (int i = 0; i < 5; i++)
+        {
+
+            if (dates.Equals(events[i, 0]))
+            {
+                Label1.Text += events[i, 1] + "<br>";
+            }
+
+        }
+        if (Label1.Text == "") Label1.Text = dates;
+    }
+
+}
