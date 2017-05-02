6b1b9f7f56916fef44acfdf95557e144ff78efac
Jess <jessallen6@hotmail.com>
2017-05-02 12:33:12 -0700

    final cut

--- assign3/Default.aspx.cs
+++ assign3/Default.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,16 +15,16 @@ public partial class _Default : System.Web.UI.Page
     protected void LoginButton_Click(object sender, EventArgs e)
     {
         int key = 0;
-        BookReviewDbEntities db = new BookReviewDbEntities();
-        int success = db.usp_ReviewerLogin(UserTextBox.Text, PasswordTextBox.Text);
+        Community_AssistEntities db = new Community_AssistEntities();
+        int success = db.usp_Login(UserTextBox.Text, PasswordTextBox.Text);
         if (success != -1)
         {
-            var uKey = (from k in db.Reviewers
-                        where k.ReviewerUserName.Equals(UserTextBox.Text)
-                        select k.ReviewerKey).FirstOrDefault();
+            var uKey = (from k in db.People
+                        where k.PersonEmail.Equals(UserTextBox.Text)
+                        select k.PersonKey).FirstOrDefault();
 
             key = (int)uKey;
-            Session["userKey"] = key;
+            Session["PersonKey"] = key;
             Response.Redirect("Author.aspx");
         }
         else
