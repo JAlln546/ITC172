48364e7138f1232e2a703db96d26cb5c87607531
Jess <jessallen6@hotmail.com>
2017-05-02 12:31:18 -0700

    Final Cut

--- assign3/Author.aspx.cs
+++ assign3/Author.aspx.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Author : System.Web.UI.Page
+public partial class AuthorPage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Session["UserKey"] == null)
+        if(Session["PersonKey"]==null)
         {
             Response.Redirect("Default.aspx");
         }
@@ -17,11 +17,17 @@ public partial class Author : System.Web.UI.Page
 
     protected void SubmitButton_Click(object sender, EventArgs e)
     {
-        BookReviewDbEntities db = new BookReviewDbEntities();
-        Author a = new Author();
-        a.AuthorName = DonationAmountTextBox.Text;
+        Community_AssistEntities db = new Community_AssistEntities();
+        Donation a = new Donation();
+        a.DonationAmount = Decimal.Parse(DonationAmountTextBox.Text);
+        a.DonationDate = DateTime.Now;
+        a.PersonKey = (int)Session["PersonKey"];
 
-        db.Authors.Add(a);
+        db.Donations.Add(a);
         db.SaveChanges();
+
+        Response.Redirect("AuthorList.aspx");
+        
+        
     }
 }
