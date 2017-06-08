f8a9c2088ff9120bd3e100be73da501967ff8fe4
Jess <jessallen6@hotmail.com>
2017-06-08 10:38:25 -0700

    get person info

---
+++ assign7/personlite.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class personlite
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string Apartment { get; set; }
+
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string HomePhone { get; set; }
+
+
+    }
+}
